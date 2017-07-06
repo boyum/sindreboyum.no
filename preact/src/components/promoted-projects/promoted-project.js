25e499b6744f5614dacfd8bfba791e3f3f1a783b
Sindre <sindre.boyum@gmail.com>
2017-07-06 06:58:53 +0200

    Renamed Image component -> AsyncImage

--- preact/src/components/promoted-projects/promoted-project.js
+++ preact/src/components/promoted-projects/promoted-project.js
@@ -1,12 +1,12 @@
 import { h, Component } from 'preact';
 import style from './style';
-import Image from '../image';
+import AsyncImage from '../async-image';
 
 export default class PromotedProject extends Component {
     render(props) {
         return(
             <div class={style.project}>
-                {/*<Image class={style.image} placeholder={props.project.img.placeholder} large={props.project.img.large} />*/}
+                {/*<AsyncImage class={style.image} placeholder={props.project.img.placeholder} large={props.project.img.large} />*/}
                 <a href={props.project.url}>
                     {props.project.name}
                 </a>
